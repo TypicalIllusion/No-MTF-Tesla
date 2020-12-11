c63b427df2cf6b5c6d0e9bff913e46a4179a9c66
TrickyBestia <thewonderfulcookie@gmail.com>
2020-12-11 10:58:46 +0300

    Made code more glamorous

--- No MTF Tesla/NMT.cs
+++ No MTF Tesla/NMT.cs
@@ -3,51 +3,43 @@
 using Exiled.API.Features;
 using Exiled.API.Enums;
 
-using PPlayer = Exiled.Events.Handlers.Player;
+using Player = Exiled.Events.Handlers.Player;
 
 namespace No_MTF_Tesla
 {
     public class NMT : Plugin<Config>
     {
-        private Handlers.Player player = new Handlers.Player();
+        private readonly Handlers.Player player = new Handlers.Player();
+
+        public static NMT Singleton { get; private set; }
         public override string Name { get; } = "NMT";
         public override string Author { get; } = "TypicalIllusion";
         public override Version Version { get; } = new Version(2, 0, 0);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 19);
         public override string Prefix { get; } = "NMT";
-
         public override PluginPriority Priority { get; } = PluginPriority.Low;
 
-
-        public static bool enabledInGame = true;
-
-        public static NMT Singleton;
-
+        public NMT()
+        {
+            Singleton = this;
+        }
         public void RegisterEvents()
         {
-            PPlayer.TriggeringTesla += player.TriggeringTeslaGate;
-            Singleton = this;
+            Player.TriggeringTesla += player.TriggeringTeslaGate;
         }
         public void UnregisterEvents()
         {
-            PPlayer.TriggeringTesla -= player.TriggeringTeslaGate;
-            player = null;
-            Singleton = null;
+            Player.TriggeringTesla -= player.TriggeringTeslaGate;
         }
-
         public override void OnEnabled()
         {
-            base.OnEnabled();
             RegisterEvents();
+            base.OnEnabled();
         }
         public override void OnDisabled()
         {
-            base.OnDisabled();
             UnregisterEvents();
-        }
-        public override void OnReloaded()
-        {
-            base.OnReloaded();
+            base.OnDisabled();
         }
     }
 }
